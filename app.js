7a5d724d7490910bdb608bd0d6a80ed6ab75bfad
anjansh <anjan.shakya@metropolia.fi>
2020-11-09 10:55:16 +0200

    mvc (currently hard coded model in array)
     better console log messages, require without .js file extension

--- app.js
+++ app.js
@@ -1,35 +1,17 @@
 'use strict';
 const express = require('express');
 const bodyParser = require('body-parser');
+const rootRoute = require('./routes/rootRoute');
+const catRoute = require('./routes/catRoute');
 const app = express();
 const port = 3000;
 
 app.use(bodyParser.urlencoded({ extended: true}));
 
-app.get('/cat', (req, res) => {
-  res.send('From this endpoint you can get cats.')
-});
-
-app.get('/cat/:id',(req, res) => {
-  console.log('http get cat with path param', req.params);
-  res.send(`From this endpoint you can get cat with id ${req.params.id}.`);
-});
+// routes
+app.use('/', rootRoute);
+app.use('/cat', catRoute);
 
 
-app.get('/',(req, res) => {
-  console.log('root route with req:', req.query);
-  res.send(`Hello world <a href="cat">click</a> <br> with test is ${req.
-      query.test} and fun is ${req.query.more}`);
-});
-
-app.post('/',(req, res) => {
-  console.log('/ route with post', req.body);
-  res.send('Hello root route with http post');
-});
-
-app.put('/', (req, res) => {
-  console.log('http put');
-  res.send('http put on root route');
-});
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
--- controllers/catController.js
+++ controllers/catController.js
@@ -1,2 +1,19 @@
 'use strict';
 // catController
+const catModel = require('../models/catModel');
+
+const cats = catModel.cats;
+
+const cat_list_get = (req, res) => {
+  res.json(cats);
+};
+
+const cat_create = (req, res) => {
+  // here we will create aat with  data coming from req..
+  res.send(`cat created with id: ...`);
+};
+
+module.exports = {
+  cat_list_get,
+  cat_create,
+};
