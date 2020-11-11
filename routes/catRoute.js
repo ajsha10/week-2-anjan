5bdde28af7e628b04a3df4818c2e69b162e2bdbb
anjansh <anjan.shakya@metropolia.fi>
2020-11-11 15:39:22 +0200

    upload folder to .gitignore, Remamber to create that folder when you deploy your app on a new server or other machine

--- routes/catRoute.js
+++ routes/catRoute.js
@@ -1,10 +1,18 @@
 'use strict';
 // catRoute
 const express = require('express');
+const multer = require('multer');
 const catController = require('../controllers/catController');
 const router = express.Router();
+const upload = multer({ dest: 'uploads/'});
 
 router.get('/', catController.cat_list_get);
+
 router.get('/:id',catController.cat_get_by_id);
+
+router.post('/', upload.single('cat'), (req, res) =>{
+  console.log('catRoute post', req.body, req.file);
+  res.send('trying to add cat ...')
+});
 
 module.exports = router;
