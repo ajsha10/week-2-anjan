8ee64badbf0a96227a11f196ee07f30904575654
anjansh <anjan.shakya@metropolia.fi>
2020-12-28 15:20:59 +0200

    https for localhost
    and hopefully production server too...

--- app.js
+++ app.js
@@ -1,21 +1,29 @@
 'use strict';
 const express = require('express');
+const cors = require('cors');
 const bodyParser = require('body-parser');
 const rootRoute = require('./routes/rootRoute');
 const catRoute = require('./routes/catRoute');
-const userRoute = require("./routes/userRoute.js");
+const userRoute = require('./routes/userRoute.js');
 const app = express();
-const port = 3000;
+
+app.use(cors());
 
 app.use(bodyParser.urlencoded({extended: true}));
+
+process.env.NODE_ENV = process.env.NODE_ENV || 'development';
+if (process.env.NODE_ENV === 'production') {
+  require('./production')(app, process.env.PORT);
+} else {
+  require('./localhost')(app, process.env.HTTPS_PORT, process.env.HTTP_PORT);
+}
 
 // routes
 app.use('/', rootRoute);
 app.use('/cat', catRoute);
-app.use("/user", userRoute);
-
-
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+app.use('/user', userRoute);
+//app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+//https.createServer(options, app).listen(httpsPort, () => console.log(`And https on port ${httpsPort}`));
 
 /*const myArr = [3, 1, 17, 42, 73, 11, 2, 7];
 const mapArr = myArr.map(i => i * 3);
